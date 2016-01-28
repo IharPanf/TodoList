8d94ad0f5ea3f497628dc2adfcd73f7328eb4324
Panfilenko <i.panfilenko@itransition.corp>
2016-01-28 18:06:38 +0300

    add header

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by i.panfilenko on 28.01.2016.
+ */
+
+(function ($) {
+
+    var tasks = [
+        { status: "new", priority: "0", date: "22/01/2016", task: "test task"},
+        { status: "new", priority: "0", date: "22/01/2016", task: "test task"},
+        { status: "new", priority: "0", date: "22/01/2016", task: "test task"},
+        { status: "new", priority: "0", date: "22/01/2016", task: "test task"},
+    ];
+} (jQuery));
+
+
