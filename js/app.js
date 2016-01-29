80de44de0134c4331e6af9f07328244755a0cd31
Panfilenko <i.panfilenko@itransition.corp>
2016-01-29 10:32:23 +0300

    use underscorejs for header and table

--- js/app.js
+++ js/app.js
@@ -2,14 +2,27 @@
  * Created by i.panfilenko on 28.01.2016.
  */
 
-(function ($) {
+$(document).ready(function(){
+    var Task = Backbone.Model.extend({
+        initialize:function(){
+            //    console.log('Model initialized');
+        }
+    });
+    var task = new Task({});
 
-    var tasks = [
-        { status: "new", priority: "0", date: "22/01/2016", task: "test task"},
-        { status: "new", priority: "0", date: "22/01/2016", task: "test task"},
-        { status: "new", priority: "0", date: "22/01/2016", task: "test task"},
-        { status: "new", priority: "0", date: "22/01/2016", task: "test task"},
+    var itemTasks = [
+        {status:"new", priority:0, datStart:'29.01.2016', textTask:'some task'},
+        {status:"new", priority:1, datStart:'28.01.2016', textTask:'some task2'},
+        {status:"new", priority:0, datStart:'27.01.2016', textTask:'some task3'},
+        {status:"new", priority:2, datStart:'26.01.2016', textTask:'some task4'}
     ];
-} (jQuery));
+
+    var template = $("#usageList").html();
+    $("#target").html(_.template(template,{itemTasks:itemTasks}));
+
+    var template =  $("#title").html();
+    $(".header").html(_.template("Simple Todo List"));
+
+})
 
 
